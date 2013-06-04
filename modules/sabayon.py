7f248152303837d2b452685277267a9df662fc2b
Andre Jaenisch <AndreJaenisch@googlemail.com>
2013-06-04 22:27:30 +0200

    Writing sabayon module for distro related stuff. Now: pastie

---
+++ modules/sabayon.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+"""
+search.py - Phenny Web Search Module
+Copyright 2008-9, Sean B. Palmer, inamidst.com
+
+Copied and changed to sabayon.py - Phenny Web Search for Sabayon.org Pastebin
+2013 by Andre Jaenisch
+Licensed under the Eiffel Forum License 2.
+
+http://inamidst.com/phenny/
+"""
+
+import urllib
+import json
+from bs4 import BeautifulSoup
+
+
+class Grab(urllib.URLopener):
+    def __init__(self, *args):
+        self.version = 'Mozilla/5.0 (Phenny)'
+        urllib.URLopener.__init__(self, *args)
+        self.addheader('Referer', 'https://github.com/sbp/phenny')
+
+    def http_error_default(self, url, fp, errcode, errmsg, headers):
+        return urllib.addinfourl(fp, [headers, errcode], "http:" + url)
+
+
+def pastie(number):
+    """Search pastebin.sabayon.org/pastie/12728 and return its JSON."""
+    """Added by Andre Jaenisch, June 2013"""
+    if isinstance(number, unicode):
+        number = number.encode('utf-8')
+    uri = 'http://pastebin.sabayon.org/pastie/'
+    arg = urllib.quote(number)
+    handler = urllib._urlopener
+    urllib._urlopener = Grab()
+    soup = BeautifulSoup(urllib.urlopen(uri + arg))
+    lis = soup.find_all('li', 'latest-pastie-item')
+    for li in lis:
+        if arg in li.a['href']:
+            wanted = li
+
+    author = wanted.b.string
+    subject = wanted.i.string
+    time = wanted.i.next_sibling.next_sibling.next_sibling.strip()
+    urllib._urlopener = handler
+    return json.dumps({'author': author,
+                       'subject': subject,
+                       'time': time,
+                       'url': uri + arg})
